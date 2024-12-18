aadd3d2954a2e665326fb46122b9eba5fa734e72
Luka Bajić <bajicluka01@gmail.com>
2024-12-18 22:03:35 +0100

    testing pca for alignment, bounding boxes for normalization

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import cv2 as cv
 import numpy as np
 import os
+from sklearn.decomposition import PCA
 #import imutils
 
 outputFolder = "output"
@@ -26,7 +27,7 @@ def writeImage(im, out):
 
 
 def findPoints(im):
-    image=im.copy()
+    image = im.copy()
     im = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
     #thresh, im_bw = cv.threshold(im, 127, 255, cv.THRESH_BINARY)
     ret, thresh = cv.threshold(im, 127, 255, 0)
@@ -38,37 +39,80 @@ def findPoints(im):
 
     #here we just take the longest contour (works fine on almost all images, but might need to be improved)
     #21L_r_4_sclera.png
-    #14R_l_4_sclera.png ...
-
+    #14R_l_4_sclera.png 
+    #36L_u_4_sclera.png
     contours = sorted(contours, key=len, reverse=True)
+    center = (-1,-1)
+
     if len(contours) != 0:
         cnt = contours[0]
         m = cv.moments(cnt)
         if m['m00'] != 0:
             centroid_x = int(m['m10']/m['m00'])
             centroid_y = int(m['m01']/m['m00'])
-            image = cv.circle(image, (centroid_x, centroid_y), 10, (0,0,255), -1)
+            center = (centroid_x, centroid_y)
+            image = cv.circle(image, center, 10, (0,0,255), -1)
+        else:
+            print("no centroid!")
+            return image, [], []
     else:
-        print("zero!")
-        return image
+        print("zero contours!")
+        return image, [], []
 
     left = tuple(cnt[cnt[:,:,0].argmin()][0])
     right = tuple(cnt[cnt[:,:,0].argmax()][0])
     top = tuple(cnt[cnt[:,:,1].argmin()][0])
     bottom = tuple(cnt[cnt[:,:,1].argmax()][0])
 
+    pois = []
+    pois.append(left)
+    pois.append(right)
+    pois.append(top)
+    pois.append(bottom)
+
     image = cv.circle(image, left, 15, (255,0,0), -1)
     image = cv.circle(image, right, 15, (255,0,0), -1)
     image = cv.circle(image, top, 15, (255,0,0), -1)
     image = cv.circle(image, bottom, 15, (255,0,0), -1)
 
-    return image
+    #testing
+    #pois = []
+    #for pt in cnt:
+    #    pois.append((pt[0][0], pt[0][1]))
 
-def alignment(im):
-    return 0
+    return image, center, pois
 
-def normalization(im):
-    return 0
+def alignment(im, center, pois):
+    #pca = PCA(n_components=2).fit(pois)
+    #print(len(pois))
+    #angle = -45
+    angle = 0
+    if(len(pois) != 0):
+        pca = PCA().fit(pois)
+        #print(pca)
+        angle1 = np.arctan2(*pca.components_[0])
+        angle2 = np.arctan2(*pca.components_[1])
+        #print(angle1, angle2)
+    else: #set a meaningless center if it wasn't successfully calculated (only relevant for 3 images in SBVPI)
+        center = (0,0)
+    rotationMatrix = cv.getRotationMatrix2D(center, angle, 1.0)
+    nrows, ncols, channels = im.shape
+    rotated = cv.warpAffine(im, rotationMatrix, (ncols, nrows))
+    return rotated
+
+def normalization(im, center, pois):
+    if len(pois) != 4:
+        print("Incorrect amount of POIs")
+        return []
+
+    #todo: do the order more elegantly
+    bb_topleft = (pois[0][0], pois[2][1])
+    bb_bottomright = (pois[1][0], pois[3][1])
+
+    print(pois)
+    print(bb_topleft, bb_bottomright)
+    cv.rectangle(im, bb_topleft, bb_bottomright, (0,0,255), 5)
+    return im
 
 def reconstruction(imgs):
     return 0
@@ -94,10 +138,12 @@ scleras = [x for x in images if "sclera" in x]
 
 for scl in scleras:
     img = cv.imread(scl)
-    img = findPoints(img)
+    img, center, pois = findPoints(img)
     #print(scl)
-    #displayImage(img)
-writeImage(img, outputFolder)
+    displayImage(img)
 
+    img = alignment(img, center, pois)
+    normalized = normalization(img, center, pois)
+    displayImage(normalized)
 
-
+#writeImage(img, outputFolder)
